46529f4303a5b3eecc5f237304533c8b8f7bd2cf
kulloveth <kulloveth@gmail.com>
2022-02-04 11:27:32 +0100

    secure the api key

---
+++ app/src/main/java/eu/tutorials/mailboxlayermailvalidation/Api.kt
@@ -0,0 +1,6 @@
+package eu.tutorials.mailboxlayermailvalidation
+
+//Todo 4: Create Api object file and add a constant value for the api key through build config
+object Api {
+    const val API_KEY = BuildConfig.API_KEY
+}
