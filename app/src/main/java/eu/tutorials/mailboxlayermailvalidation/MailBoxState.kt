b2a8b2786d6c65c833266654cfa8940917d0ffa8
kulloveth <kulloveth@gmail.com>
2022-02-07 11:53:03 +0100

    creating the state class and connecting to the ui

--- app/src/main/java/eu/tutorials/mailboxlayermailvalidation/MailBoxState.kt
+++ app/src/main/java/eu/tutorials/mailboxlayermailvalidation/MailBoxState.kt
@@ -1,4 +1,66 @@
 package eu.tutorials.mailboxlayermailvalidation
 
-class MailBoxState {
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+/** Todo 6: We initialize the State class using remember function and pass in rememberCoroutineScope
+ *  as argument. The remember function will call the class again when there is need for recomposition
+ */
+@Composable
+fun rememberMailBoxState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+)
+        =     remember(coroutineScope) {
+    MailBoxState( coroutineScope = coroutineScope)
+}
+
+
+//Todo 1: Create a state class with coroutine scope as parameter for launching the suspend function
+class MailBoxState(private val coroutineScope: CoroutineScope) {
+
+    /** Todo 2: Create a MutableStateFlow variable for setting the response and StateFlow
+     *   that is not mutable for exposing the response to the UI
+     */
+    private val _response  = MutableStateFlow(EmailResponse())
+    val response:StateFlow<EmailResponse>
+    get() = _response
+
+    /** Todo 3: create a MutableStateFlow variable for setting the validity response and StateFlow
+     *   that is not mutable for exposing  the validity to the UI. With StateFlow, UI elements can
+     *   react to changes to data and recompose to update the value
+     * */
+    private val _valid  = MutableStateFlow("")
+    val valid:StateFlow<String>
+    get() = _valid
+
+    /** Todo 4: A function that receives the email to be checked. Using the coroutineScope, we launch
+     *   the suspend function from the service class within a try catch block to check the email state.
+     *   We pass in emailState as argument for email and the api key as argument for key and set the response returned
+     *   from the request to _response state variable
+     *
+     * */
+    fun checkEmail(emailState: String){
+        try {
+            coroutineScope.launch {
+                _response.value =
+                    Api.service.checkEmail(email = emailState, key = Api.API_KEY)
+                Log.d("email2","${response.value}")
+                /** Todo 5: In the EmailResponse class we have a format_valid property that returns a boolean.
+                 *  We check if this is true we know the format is valid but if it is false
+                 *  which is the default that we have set, then it is invalid.
+                 *  We set this result to _valid state variable
+                 * */
+                _valid.value = if (response.value.format_valid) "Email is valid" else "Invalid email"
+            }
+        }catch (e: Exception){
+
+        }
+    }
 }
--- app/src/main/java/eu/tutorials/mailboxlayermailvalidation/MainActivity.kt
+++ app/src/main/java/eu/tutorials/mailboxlayermailvalidation/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,10 +19,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //Todo 9: create access to the state class by assigning it to a variable
+            val appState = rememberMailBoxState()
+
+            //Todo 10: collect the value from the returned response and assign to a variable
+            val response  =
+                appState.response.collectAsState()
+
+            //Todo 11: create a remember variable for the email
+            var emailState by remember {
+                mutableStateOf("")
+            }
             MailBoxLayerMailValidationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    /** Todo 12: Set emailState as argument for value, set a lambda block
+                     * for onValueChange and receive the value from it and assign to emailState.
+                     * In onButtonClicked, we check that emailState is not empty and call the
+                     * checkEmail function with emailState assigned as parameter.
+                     * Then we set appState as argument to state
+                     * */
+                    EmailScreen(value = emailState, onValueChange = {
+                        emailState  = it
+                    }, onButtonClicked = {
+                        if (emailState.isNotEmpty()) {
+                            appState.checkEmail(emailState = emailState)
+                        }
+                    }, state = appState)
                 }
             }
         }
@@ -34,7 +57,9 @@ class MainActivity : ComponentActivity() {
 fun EmailScreen(
    value: String,
                 onValueChange: (String) -> Unit={},
-    onButtonClicked:()->Unit={}) {
+    onButtonClicked:()->Unit={},
+   //Todo 7: create a state variable
+   state: MailBoxState) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -43,7 +68,9 @@ fun EmailScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(value = value, onValueChange = onValueChange)
-        Text(text = "Invalid Email", modifier = Modifier.padding(vertical = 8.dp))
+        Text(
+            //Todo 8: update the validity text using the valid value for the State class
+            text = state.valid.collectAsState().value, modifier = Modifier.padding(vertical = 8.dp))
         OutlinedButton(onClick = onButtonClicked) {
             Text(text = "Check Email")
         }
@@ -54,6 +81,6 @@ fun EmailScreen(
 @Composable
 fun DefaultPreview() {
     MailBoxLayerMailValidationTheme {
-        EmailScreen(value = "")
+        EmailScreen(value = "", state = rememberMailBoxState())
     }
 }
